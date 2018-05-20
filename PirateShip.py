7b8f150d82faec7e5fcc2fe6732871b4bbe4679f
Vyse <vyse323@gmail.com>
2018-05-20 12:16:38 -0700

    Fixed issues with running individual files; Made application independent of calling directory Changed broadcast frequency to a silent channel (thank god one exists in AZ) Added scaffolding for changing broadcast frequency function Removed unnecessary comments

--- PirateShip.py
+++ PirateShip.py
@@ -3,9 +3,10 @@ import os
 import random
 
 # Globals
-AUDIO_FILES_DIRECTORY = "./music/"
-
-BROADCAST_FREQUENCY = "93.8"
+RUNTIME_DIR = "/home/pi/PirateShip/"
+AUDIO_FILES_DIRECTORY = RUNTIME_DIR + "music/"
+PI_FM_RDS = RUNTIME_DIR + "dependencies/core/pi_fm_rds"
+BROADCAST_FREQUENCY = "107.5"
 
 def main():
     # Do stuff
@@ -17,9 +18,9 @@ def build_playlist():
     files = os.listdir(AUDIO_FILES_DIRECTORY)
     random.shuffle(files)
 
-    with open("playlist.m3u", "w") as file:
+    with open(RUNTIME_DIR + "playlist.m3u", "w") as file:
         for filename in files:
-            # Skip comments
+            # Skip comment file
             if filename.startswith('#'):
                 continue
             else:
@@ -29,23 +30,33 @@ def build_playlist():
 def play_playlist():
     print("Playing playlist on frequency " + BROADCAST_FREQUENCY)
 
+    # Alternative audio processing, not sure if it will function with PI_FM_RDS but it might with some tweaking
     #ps = subprocess.Popen(["ffmpeg", "-nostdin", "-i", file, "-f", "s16le", "-ar", "22.05k", "-ac", "1", "-"], stdout=subprocess.PIPE)
-    # CONFIRMED WOKRING SETTINGS
-    # ps = subprocess.Popen(['sox', '-v', '.9', '-t', 'mp3', './playlist.m3u', '-t', 'wav', '--input-buffer', '80000', '-r', '22050', '-c', '2', '-'], stdout=subprocess.PIPE)
 
-    # Experimental
-    #ps = subprocess.Popen(['sox', '-v', '.9', '-t', 'mp3', './playlist.m3u', '-t', 'wav', '--input-buffer', '80000', '-r', '44100', '-c', '2', '-'], stdout=subprocess.PIPE)
-    with open("playlist.m3u", "r") as file:
+    with open(RUNTIME_DIR + "playlist.m3u", "r") as file:
+        # Create the PI FM RDS subprocess asynchronously so we can bind the sox stdout to this subprocess input pipe below.
+        output = subprocess.Popen(["sudo", PI_FM_RDS, "-freq", BROADCAST_FREQUENCY, "-audio", "-"], stdin=subprocess.PIPE, stdout=subprocess.DEVNULL)
+
         for filename in file:
-            ps = subprocess.Popen(['sox', '-t', 'mp3', filename.rstrip(), '-t', 'wav', '--input-buffer', '80000', '-'], stdout=subprocess.PIPE)
+
+            if output.poll() is not None:
+                # In case for whatever reason the subprocess gets killed after the initial processing, spawn a new one.
+                output = subprocess.Popen(["sudo", PI_FM_RDS, "-freq", BROADCAST_FREQUENCY, "-audio", "-"],stdin=subprocess.PIPE, stdout=subprocess.DEVNULL)
+
+            # Processes mp3 to stdout (piped into the PI_FM_RDS process) using an input buffer.
+            ps = subprocess.Popen(['sox', '-t', 'mp3', (filename.rstrip()), '-t', 'wav', '--input-buffer', '80000', '-'], stdout=output.stdin)
+
             # TODO: display what song is playing on the PS and RT range
             # https://github.com/ChristopheJacquet/PiFmRds
-            output = subprocess.Popen(["sudo", "./dependencies/core/pi_fm_rds", "-freq", BROADCAST_FREQUENCY, "-audio", "-"], stdin=ps.stdout)
-            print("Closing sox stdout")
-            print("Waiting on sox process to finish")
+
+            print("Processing song: "+filename+"waiting for sox to complete")
             ps.wait()
-            print("Transmission broadcasting")
-            output.wait()
+
+        print("Playlist complete, waiting for transmission to complete")
+        output.wait()
+
+def change_broadcast_frequency():
+    raise NotImplementedError
 
 
 # "ffmpeg -i "+arg.song_file+" "+"-f s16le -ar 22.05k -ac 1 - | sudo ./fm_transmitter -f"+" "+frequency+" "+" - "
